09284c3d7eabb73a4f65a155d1dd9731f5b94b9a
Ewan-Selkirk <ewanselkirk@sky.com>
2022-07-15 21:38:58 +0100

    More bug fixes + Pycord 2.0.0 Update Added catch for pinned messages as they would cause the command to break. Added check for if the user is still in the guild when using the rejoin option of 'channel_to_thread'. Changed many imports to not require the Discord namespace. Changed the create thread button to add the creator of the thread to it. Changed method of deleting webhook to run even if the command fails.

--- bot.py
+++ bot.py
@@ -2,8 +2,9 @@ import json
 import os
 
 import discord
+from discord import ButtonStyle
 from discord.commands import Option
-from discord.ui import InputText, Modal, View
+from discord.ui import InputText, Modal, View, Button, button
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -22,6 +23,7 @@ class ThreadModal(Modal):
         thread = await interaction.channel.create_thread(name=self.children[0].value,
                                                          type=discord.ChannelType.public_thread)
         await interaction.response.send_message(f"Created thread `{self.children[0].value}`", ephemeral=True)
+        await thread.add_user(interaction.user)
         await thread.remove_user(bot.user)
 
 
@@ -55,10 +57,10 @@ async def channel_to_thread(ctx, channel: Option(discord.TextChannel, "Select a 
         await ctx.followup.send("Could not convert channel.\nIs the channel private?")
         return
 
-    try:
-        # Create webhook to emulate users posting in channels
-        hook = await place.create_webhook(name="Channel to Thread [DELETE IF FOUND]", reason="Moving channel to thread")
+    # Create webhook to emulate users posting in channels
+    hook = await place.create_webhook(name="Channel to Thread [DELETE IF FOUND]", reason="Moving channel to thread")
 
+    try:
         # Create a new thread
         thread = await place.create_thread(name=name or str(channel), type=discord.ChannelType.public_thread,
                                            reason=f"Moved channel: `{channel}` to a thread")
@@ -69,25 +71,31 @@ async def channel_to_thread(ctx, channel: Option(discord.TextChannel, "Select a 
         for msg in hist:
             # Media that is marked as spoilers will need to be reuploaded due to bots not embedding spoilered URLs.
             # Any other media should be linked to just fine, working around the issue of Nitro uploads
-            await hook.send(msg.content + '\n'.join([f.url for f in msg.attachments if not f.is_spoiler()]),
-                            username=msg.author.display_name if type(msg.author) == discord.Member else msg.author.name,
-                            avatar_url=msg.author.display_avatar.url, embeds=msg.embeds, thread=thread,
-                            files=[await f.to_file() for f in msg.attachments if f.is_spoiler()])
+            try:
+                await hook.send(msg.content + '\n'.join([f.url for f in msg.attachments if not f.is_spoiler()]),
+                                username=msg.author.display_name if type(msg.author) == discord.Member
+                                else msg.author.name, avatar_url=msg.author.display_avatar.url,
+                                embeds=msg.embeds, thread=thread,
+                                files=[await f.to_file() for f in msg.attachments if f.is_spoiler()])
+            # Pinned message notifications will be caught as messages with no content
+            except discord.errors.HTTPException as err:
+                print(type(err), err)
 
         if rejoin:
             for msg in hist:
-                if msg.author not in thread.members:
+                if msg.author not in thread.members and msg.author in ctx.guild.members:
                     await thread.add_user(msg.author)
 
         if remove:
             await channel.delete()
 
         await thread.remove_user(bot.user)
-        await hook.delete(reason=f"Moved channel `{channel}` to a thread in `{place}`. No longer needed")
         await ctx.followup.send(f"Moved channel `{channel}` to a thread in `{place}`")
     except TypeError or discord.DiscordException as err:
         print(type(err), err)
         await ctx.followup.send(f"Could not move channel to thread\n{type(err)} {err}")
+    finally:
+        await hook.delete(reason=f"Moved channel `{channel}` to a thread in `{place}`. No longer needed")
 
 
 # @bot.slash_command(name="remove_all_threads", guild_ids=GUILD, default_permission=False,
@@ -113,9 +121,7 @@ class btn_createThread(View, discord.TextChannel):
     def __init__(self):
         super().__init__(timeout=None)
 
-    @discord.ui.button(label="Create new thread",
-                       style=discord.ButtonStyle.green, emoji="➕",
-                       custom_id=f"button_newThread")
+    @button(label="Create new thread", style=ButtonStyle.green, emoji="➕", custom_id=f"button_newThread")
     async def button_callback(self, button, interaction):
         modal = ThreadModal(title="New Thread", custom_id=f"threadModal_newThread")
         await interaction.response.send_modal(modal)
